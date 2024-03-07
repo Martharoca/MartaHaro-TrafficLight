1e9198ec6d94e6070f0a09774287bdefad58defb
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-03-07 09:51:06 +0000

    traffic-light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,11 @@
-// import React, {useState} from "react";
+
 import React, { useState } from "react";
 
 
 //create your first component
 const Home = () => {
 	const [color, setColor] = useState("red");
-
+// las llaves <>  al principio de la clase "traffic-top" me aparecen automaticamente cuando le digo a codespace que me lo ponga formato jsx
 	return (
 		<><div className="traffic-top"></div>
 		<div className="traffic-light">
