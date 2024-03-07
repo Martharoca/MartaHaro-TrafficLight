386f2c8f9831c20469d0a4575871d1fe5c28b9ff
Marta Haro <143503919+Martharoca@users.noreply.github.com>
2024-03-07 09:47:38 +0000

    trafficLight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,14 +7,15 @@ const Home = () => {
 	const [color, setColor] = useState("red");
 
 	return (
+		<><div className="traffic-top"></div>
 		<div className="traffic-light">
 			<div onClick={() => setColor("red")}
-			className={"red" + (color === "red" ? " glow" : "")}></div>
+				className={"red" + (color === "red" ? " glow" : "")}></div>
 			<div onClick={() => setColor("yellow")}
-			className={"yellow" + (color === "yellow" ? " glow" : "")}></div>
+				className={"yellow" + (color === "yellow" ? " glow" : "")}></div>
 			<div onClick={() => setColor("green")}
-			className={"green" + (color === "green" ? " glow" : "")}></div>
-		</div>
+				className={"green" + (color === "green" ? " glow" : "")}></div>
+		</div></>
 	);
 };
 
